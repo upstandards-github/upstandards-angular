afa043a48196560d4efeb66ce7cbfa53d32d38f7
Subrat Kumar Bhola <46965727+subratbhola@users.noreply.github.com>
2023-05-07 22:23:58 +0530

    form validation

--- src/app/forms/template-driven-form/template-driven-form.component.ts
+++ src/app/forms/template-driven-form/template-driven-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
