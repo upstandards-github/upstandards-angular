faee4347613d436de6b02c46f5c9bba13bd3f3b6
Subrat Kumar Bhola <46965727+subratbhola@users.noreply.github.com>
2023-05-16 08:59:51 +0530

    reactive form

--- src/app/forms/reactive-form/reactive-form.component.ts
+++ src/app/forms/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class ReactiveFormComponent {
 
+  userForm = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl('Bhola'),
+    address: new FormGroup({
+      state: new FormControl(''),
+      city: new FormControl(''),
+    })
+  })
+
+  updateForm() {
+    this.userForm.patchValue({
+      firstname:'subrat',
+      address:{
+        state:'Odisha',
+        city:'berhampur'
+      }
+    })
+  }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    public name: string | undefined;
+    public email: string | undefined;
+    public username: string | undefined;
+    public password: string | undefined;
+    public confirmPassword: string | undefined;
+}
